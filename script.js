bf92ffc2d4f8573583341db2cb411fb51e293d30
JaneCortes <janeth.150999@gmail.com>
2025-08-02 15:41:07 -0600

    Se corrigio boton primario y se agrego oscuridad

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
-const hamburger = document.getElementById('hamburger');
-const navMenu = document.getElementById('nav-menu');
-const links = document.querySelectorAll('nav ul li a');
+document.addEventListener('DOMContentLoaded', () => {
+  const hamburger = document.getElementById('hamburger');
+  const navMenu = document.getElementById('nav-menu');
+  const links = document.querySelectorAll('nav ul li a');
 
-hamburger.addEventListener('click', () => {
-  navMenu.classList.toggle('active');
-});
+  hamburger.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+  });
 
-// cerrar menú al seleccionar cualquier enlace
-links.forEach(link => {
-  link.addEventListener('click', () => {
-    // cambiar activo
-    links.forEach(l => l.classList.remove('active'));
-    link.classList.add('active');
+  links.forEach(link => {
+    link.addEventListener('click', () => {
+      links.forEach(l => l.classList.remove('active'));
+      link.classList.add('active');
 
-    // cerrar el menú si está abierto (solo en móvil)
-    if (navMenu.classList.contains('active')) {
-      navMenu.classList.remove('active');
-    }
+      if (navMenu.classList.contains('active')) {
+        navMenu.classList.remove('active');
+      }
+    });
   });
 });
